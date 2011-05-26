c3a99e66c5bfdee9e36493574529eb7d0c295707
Sebastien Binet <binet@farnsworth>
2011-05-26 18:55:39 +0200

    added the decoder

--- pkg/ctypes/ctypes.go
+++ pkg/ctypes/ctypes.go
@@ -114,6 +114,13 @@ type Value struct {
 	cstrings map[int]cstring // a pool of C-string we own. index is the offset in the Value.b buffer
 }
 
+// ValueOf returns the ctypes.Value corresponding to the Go-value v
+func ValueOf(v interface{}) *Value {
+	rv := reflect.ValueOf(v).Elem()
+	ct := gotype_to_ctype(rv.Type())
+	return New(ct)
+}
+
 func New(t Type) *Value {
 	if t == nil {
 		panic("ctypes: New(nil)")
@@ -166,8 +173,14 @@ type doublecomplex struct {
 	imag float64
 }
 
+// get the C type corresponding to a Go value
+func TypeOf(v interface{}) Type {
+	rt := reflect.TypeOf(v)
+	return gotype_to_ctype(rt)
+}
+
 // get the C type corresponding to a Go type
-func TypeOf(t reflect.Type) Type {
+func gotype_to_ctype(t reflect.Type) Type {
 	switch t.Kind() {
 	case reflect.Bool,
 		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
@@ -220,7 +233,7 @@ func (t *common_type) Kind() Kind {
 }
 
 func (t *common_type) Elem() Type {
-	return TypeOf(t.Type.Elem())
+	return gotype_to_ctype(t.Type.Elem())
 }
 
 func (t *common_type) Field(i int) (c StructField) {
@@ -228,7 +241,7 @@ func (t *common_type) Field(i int) (c StructField) {
 	c = StructField{
 		PkgPath: f.PkgPath,
 		Name:    f.Name,
-		Type:    TypeOf(f.Type),
+		Type:    gotype_to_ctype(f.Type),
 		Tag:     f.Tag,
 		// FIXME?: this should be corrected for vlarrays/cstrings
 		Offset: f.Offset,
@@ -248,7 +261,8 @@ type vlarray_type struct {
 }
 
 func (t *vlarray_type) Size() uintptr {
-	return reflect.TypeOf(uintptr(0)).Size() + reflect.TypeOf(int(0)).Size()
+	sz := reflect.TypeOf(uintptr(0)).Size() + reflect.TypeOf(int(0)).Size()
+	return sz
 }
 
 type cstring_type struct {
@@ -276,9 +290,25 @@ func new_cstruct(t reflect.Type) *cstruct_type {
 	nfields := t.NumField()
 	for i := 0; i < nfields; i++ {
 		f := t.Field(i)
-		c.fields[f.Name] = TypeOf(f.Type)
+		c.fields[f.Name] = gotype_to_ctype(f.Type)
 	}
 	return c
+}
+
+func (t *cstruct_type) Field(i int) (c StructField) {
+	f := t.Type.Field(i)
+	c = StructField{
+		PkgPath: f.PkgPath,
+		Name:    f.Name,
+		Type:    gotype_to_ctype(f.Type),
+		Tag:     f.Tag,
+		// FIXME?: this should be corrected for vlarrays/cstrings
+		Offset: f.Offset,
+		// FIXME?: ditto
+		Index:     f.Index,
+		Anonymous: f.Anonymous,
+	}
+	return
 }
 
 func (t *cstruct_type) Size() uintptr {
@@ -290,47 +320,6 @@ func (t *cstruct_type) Size() uintptr {
 	return sz
 }
 
-
-// An Encoder is bound to a particular reflect.Type and knows how to
-// convert a Go value into a ctypes.Value
-type Encoder interface {
-	Encode(v interface{}) (*Value, os.Error)
-}
-
-func Encode(v interface{}) *Value {
-	rv := reflect.ValueOf(v).Elem()
-	rt := reflect.TypeOf(v).Elem()
-	t := TypeOf(rt)
-	c_value := New(t)
-
-	encode_value(c_value, rv)
-	return c_value
-}
-
-type ctype_encoder struct {
-	v *Value // the C-value in which we encode
-}
-
-// Create a new encoder bound to the Go-type t
-func NewEncoder(t interface{}) Encoder {
-	rt := reflect.TypeOf(t).Elem()
-	ct := TypeOf(rt)
-	cv := New(ct)
-	return &ctype_encoder{v: cv}
-}
-
-// Encode a Go value into a ctypes.Value
-func (e *ctype_encoder) Encode(v interface{}) (*Value, os.Error) {
-	rt := reflect.TypeOf(v).Elem()
-	if rt != e.v.Type().GoType() {
-		return nil, os.NewError("cannot encode this type [" + rt.String() + "]")
-	}
-
-	rv := reflect.ValueOf(v).Elem()
-	e.v.Reset()
-	encode_value(e.v, rv)
-	return e.v, nil
-}
 
 const (
 	sz_bool = unsafe.Sizeof(bool(true))
@@ -356,8 +345,35 @@ const (
 	sz_complex128 = unsafe.Sizeof(complex(float64(0), float64(0)))
 )
 
-type enc_op func(v *Value, p unsafe.Pointer)
+// An Encoder is bound to a particular reflect.Type and knows how to
+// convert a Go value into a ctypes.Value
+type Encoder interface {
+	Encode(v interface{}) (*Value, os.Error)
+}
 
+type ctype_encoder struct {
+	v *Value // the C-value in which we encode
+}
+
+// Create a new encoder bound to the C-value v
+func NewEncoder(v *Value) Encoder {
+	return &ctype_encoder{v: v}
+}
+
+// Encode a Go value into a ctypes.Value
+func (e *ctype_encoder) Encode(v interface{}) (*Value, os.Error) {
+	rt := reflect.TypeOf(v).Elem()
+	if rt != e.v.Type().GoType() {
+		return nil, os.NewError("cannot encode this type [" + rt.String() + "]")
+	}
+
+	rv := reflect.ValueOf(v).Elem()
+	e.v.Reset()
+	encode_value(e.v, rv)
+	return e.v, nil
+}
+
+type enc_op func(v *Value, p unsafe.Pointer)
 var enc_op_table []enc_op
 
 func encode_noop(v *Value, p unsafe.Pointer) {
@@ -534,6 +550,216 @@ func encode_value(cv *Value, rv reflect.Value) {
 	}
 }
 
+// A Decoder is bound to a particular reflect.Type and knows how to
+// convert a ctypes.Value into a Go-value
+type Decoder interface {
+	Decode(v interface{}) (*Value, os.Error)
+}
+
+type ctype_decoder struct {
+	v *Value // the C-value from which we decode
+}
+
+// Create a new decoder bound to the c-value v
+func NewDecoder(v *Value) Decoder {
+	v.idx = 0
+	return &ctype_decoder{v: v}
+}
+
+// Decode a ctypes.Value into a Go value
+func (d *ctype_decoder) Decode(v interface{}) (*Value, os.Error) {
+	rt := reflect.TypeOf(v).Elem()
+	if rt != d.v.Type().GoType() {
+		return nil, os.NewError("cannot decode this type [" + rt.String() + "]")
+	}
+
+	rv := reflect.ValueOf(v).Elem()
+	decode_value(d.v, rv)
+	return d.v, nil
+}
+
+type dec_op func(v *Value, p unsafe.Pointer)
+var dec_op_table []dec_op
+
+func decode_noop(v *Value, p unsafe.Pointer) {
+	panic("noop!")
+}
+
+func decode_bool(v *Value, p unsafe.Pointer) {
+	src := (*bool)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*bool)(p)
+	*dst = *src
+	v.idx += sz_bool
+}
+
+func decode_int(v *Value, p unsafe.Pointer) {
+	src := (*int)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*int)(p)
+	*dst = *src
+	v.idx += sz_int
+}
+
+func decode_int8(v *Value, p unsafe.Pointer) {
+	src := (*int8)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*int8)(p)
+	*dst = *src
+	v.idx += sz_int8
+}
+
+func decode_int16(v *Value, p unsafe.Pointer) {
+	src := (*int16)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*int16)(p)
+	*dst = *src
+	v.idx += sz_int16
+}
+
+func decode_int32(v *Value, p unsafe.Pointer) {
+	src := (*int32)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*int32)(p)
+	*dst = *src
+	v.idx += sz_int32
+}
+
+func decode_int64(v *Value, p unsafe.Pointer) {
+	src := (*int64)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*int64)(p)
+	*dst = *src
+	v.idx += sz_int64
+}
+
+func decode_uint(v *Value, p unsafe.Pointer) {
+	src := (*uint)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*uint)(p)
+	*dst = *src
+	v.idx += sz_uint
+}
+
+func decode_uint8(v *Value, p unsafe.Pointer) {
+	src := (*uint8)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*uint8)(p)
+	*dst = *src
+	v.idx += sz_uint8
+}
+
+func decode_uint16(v *Value, p unsafe.Pointer) {
+	src := (*uint16)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*uint16)(p)
+	*dst = *src
+	v.idx += sz_uint16
+}
+
+func decode_uint32(v *Value, p unsafe.Pointer) {
+	src := (*uint32)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*uint32)(p)
+	*dst = *src
+	v.idx += sz_uint32
+}
+
+func decode_uint64(v *Value, p unsafe.Pointer) {
+	src := (*uint64)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*uint64)(p)
+	*dst = *src
+	v.idx += sz_uint64
+}
+
+func decode_uintptr(v *Value, p unsafe.Pointer) {
+	src := (*uintptr)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*uintptr)(p)
+	*dst = *src
+	v.idx += sz_uintptr
+}
+
+func decode_float32(v *Value, p unsafe.Pointer) {
+	src := (*float32)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*float32)(p)
+	*dst = *src
+	v.idx += sz_float32
+}
+
+func decode_float64(v *Value, p unsafe.Pointer) {
+	src := (*float64)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*float64)(p)
+	*dst = *src
+	v.idx += sz_float64
+}
+
+func decode_complex64(v *Value, p unsafe.Pointer) {
+	src := (*complex64)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*complex64)(p)
+	*dst = *src
+	v.idx += sz_complex64
+}
+
+func decode_complex128(v *Value, p unsafe.Pointer) {
+	src := (*complex128)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*complex128)(p)
+	*dst = *src
+	v.idx += sz_complex128
+}
+
+func decode_array(v *Value, p unsafe.Pointer) {
+	arr := (*reflect.Value)(p)
+	op := dec_op_table[arr.Type().Elem().Kind()]
+
+	length := arr.Len()
+	for i := 0; i < length; i++ {
+		dst := unsafe.Pointer(arr.Index(i).UnsafeAddr())
+		op(v, dst)
+	}
+}
+
+func decode_ptr(v *Value, p unsafe.Pointer) {
+	src := (*uintptr)(unsafe.Pointer(&v.b[v.idx]))
+	dst := (*uintptr)(p)
+	*dst = *src
+	v.idx += sz_uintptr
+}
+
+func decode_slice(v *Value, p unsafe.Pointer) {
+	slice := (*reflect.SliceHeader)(p)
+	decode_int(v, unsafe.Pointer(&slice.Len))
+	decode_ptr(v, unsafe.Pointer(&slice.Data))
+}
+
+func decode_string(v *Value, p unsafe.Pointer) {
+	
+	s := C.GoString(v.cstrings[v.idx])
+	src := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	dst := (*reflect.StringHeader)(p)
+	dst.Data = src.Data
+	dst.Len  = src.Len
+	//decode_ptr(v, unsafe.Pointer(cstr))
+	v.idx += sz_uintptr
+}
+
+func decode_struct(v *Value, p unsafe.Pointer) {
+	rv := (*reflect.Value)(p)
+	nfields := rv.NumField()
+	for i := 0; i < nfields; i++ {
+		f := rv.Field(i)
+		decode_value(v, f)
+	}
+}
+
+func decode_value(cv *Value, rv reflect.Value) {
+
+	kind := rv.Type().Kind()
+	op := dec_op_table[kind]
+	switch kind {
+	default:
+		op(cv, unsafe.Pointer(rv.UnsafeAddr()))
+	case reflect.Array:
+		op(cv, unsafe.Pointer(&rv))
+	case reflect.Ptr:
+		op(cv, unsafe.Pointer(rv.UnsafeAddr()))
+	case reflect.Slice, reflect.Struct:
+		op(cv, unsafe.Pointer(&rv))
+	case reflect.String:
+		op(cv, unsafe.Pointer(rv.UnsafeAddr()))
+	}
+}
+
+
 func init() {
 	enc_op_table = []enc_op{
 		reflect.Bool:       encode_bool,
@@ -561,6 +787,34 @@ func init() {
 		reflect.Slice:      encode_slice,
 		reflect.String:     encode_string,
 		reflect.Struct:     encode_struct,
+	}
+
+	dec_op_table = []dec_op{
+		reflect.Bool:       decode_bool,
+		reflect.Int:        decode_int,
+		reflect.Int8:       decode_int8,
+		reflect.Int16:      decode_int16,
+		reflect.Int32:      decode_int32,
+		reflect.Int64:      decode_int64,
+		reflect.Uint:       decode_uint,
+		reflect.Uint8:      decode_uint8,
+		reflect.Uint16:     decode_uint16,
+		reflect.Uint32:     decode_uint32,
+		reflect.Uint64:     decode_uint64,
+		reflect.Uintptr:    decode_uintptr,
+		reflect.Float32:    decode_float32,
+		reflect.Float64:    decode_float64,
+		reflect.Complex64:  decode_complex64,
+		reflect.Complex128: decode_complex128,
+		reflect.Array:      decode_array,
+		reflect.Chan:       decode_noop,
+		reflect.Func:       decode_noop,
+		reflect.Interface:  decode_noop,
+		reflect.Map:        decode_noop,
+		reflect.Ptr:        decode_ptr,
+		reflect.Slice:      decode_slice,
+		reflect.String:     decode_string,
+		reflect.Struct:     decode_struct,
 	}
 }
 
